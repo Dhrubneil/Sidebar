d99cab690c733b2eb9d607f50917f2b411ca1f60
Dhrubneil <62657712+Dhrubneil@users.noreply.github.com>
2021-09-29 19:12:35 +0800

    Add files via upload

---
+++ new.js
@@ -0,0 +1,25 @@
+let cogsDetail = document.getElementById('cogsDetail');
+let headphonesDetail = document.getElementById('headphonesDetail');
+let keyboardDetail = document.getElementById('keyboardDetail');
+let laptopDetail = document.getElementById('laptopDetail');
+let tabletDetail = document.getElementById('tabletDetail');
+let mobileDetail = document.getElementById('mobileDetail');
+let cubeDetail = document.getElementById('cubeDetail');
+let highlighterDetail = document.getElementById('highlighterDetail');
+let paperPlaneDetail = document.getElementById('paperPlaneDetail');
+let paperClipDetail = document.getElementById('paperClipDetail');
+let archiveDetail = document.getElementById('archiveDetail');
+
+
+
+// cogsDetail.innerHTML = "cogs";
+// headphonesDetail.innerHTML = "headphones";
+// keyboardDetail.innerHTML = "keyboard";
+// laptopDetail.innerHTML = "laptop";
+// tabletDetail.innerHTML = "tablet";
+// mobileDetail.innerHTML = "phone";
+// cubeDetail.innerHTML = "cube";
+// highlighterDetail.innerHTML = "highlighter";
+// paperPlaneDetail.innerHTML = "paper-plane";
+// paperClipDetail.innerHTML = "paper-clip";
+// archiveDetail.innerHTML = "archive";
